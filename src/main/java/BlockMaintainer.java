c1e10f18b16bf79dff672f922fe6b42e2b2cbfe4
Bhavadharani <bhavadharaniks@gmail.com>
2020-02-11 14:56:54 +0530

    ADD | Block which have a row and column and blockmaintainer can check whether two rows of a block are equal

---
+++ src/main/java/BlockMaintainer.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class BlockMaintainer {
+
+    private List<Block> blockList;
+
+    public BlockMaintainer(List<Block> blockList) {
+
+        this.blockList = blockList;
+    }
+
+    public boolean areRowsEqual() {
+
+        return true;
+    }
+}
